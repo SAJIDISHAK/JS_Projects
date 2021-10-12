ffc95bb7c2e3004ef93bb08613c74aa39857becc
Sajid Ishaq <SAJIDISHAK7@GMAIL.COM>
2021-10-13 01:56:54 +0500

    Project 1 JS
    Added required field Validation

---
+++ project 1/app.js
@@ -0,0 +1,63 @@
+// Receiving HTML elements from the DOM
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+//Function to update class and message for errors
+function showError(input, message) {
+    //Get the parent element to the input feild {.form-controll}
+    const formControl = input.parentElement;
+    //Replace the class - add error
+    formControl.className = "form-controll error";
+    //Get the small element for the error message
+    const small = formControl.querySelector("small");
+    //Override the text for small element using the input message
+    small.innerText = message;
+}
+
+//Funtion to update class for success
+function showSuccess(input) {
+    //Get the parent element to the input feild {.form-controll}
+    const formControl = input.parentElement;
+    //Replace the class - add error
+    formControl.className = "form-controll success";
+
+}
+
+//Event Listeners
+//create event listner for submit button
+form.addEventListener("submit", function (e) {
+    //stop page from reloading on submit
+    e.preventDefault();
+
+    //Check to see if fields meet required field requirement 
+    // Check if username is empty
+    if (username.value === "") {
+        showError(username, "Username is Required")
+    } else {
+        showSuccess(username)
+    }
+
+    // Check if email is empty
+    if (email.value === "") {
+        showError(email, "Email is Required")
+    } else {
+        showSuccess(email)
+    }
+
+    // Check if password is empty
+    if (password.value === "") {
+        showError(password, "Password is Required")
+    } else {
+        showSuccess(password)
+    }
+
+    // Check if password is empty
+    if (password2.value === "") {
+        showError(password2, "Confirm Password is Required")
+    } else {
+        showSuccess(password2)
+    }
+});
