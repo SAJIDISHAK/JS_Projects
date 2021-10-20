c7e3300c298d1a2ab9ffd7fc7c7cd7ab64aa1998
Sajid Ishaq <SAJIDISHAK7@GMAIL.COM>
2021-10-20 21:02:02 +0500

    Functionality Complete

--- project 2/app.js
+++ project 2/app.js
@@ -5,7 +5,7 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-const ticketPrice = movieSelect.value;
+let ticketPrice = movieSelect.value;
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
@@ -14,11 +14,17 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+//Event Listeners
+// 1. Event Listener for container to check for click on seats
 container.addEventListener("click", e => {
     if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
         e.target.classList.toggle("selected");
         updateSelectedCount();
-    } else {
-
     }
-})
+});
+
+// 2. Event listener for movie select
+movieSelect.addEventListener("change", e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
